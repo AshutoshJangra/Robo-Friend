9fb0182814871a6d9eff7af113b0c0bf5d7d3be8
ashutoshjangra <ashutoshjangra603@gmail.com>
2018-12-29 11:26:47 +0530

    modifying ui

--- src/App.js
+++ src/App.js
@@ -1,46 +1,45 @@
 import React, { Component } from "react";
 import { Robots } from "./Robots";
 import CardList from "./CardList";
-import SearchBox from "../src/Components/SearchBox"
+import Scroll from './Components/Scroll'
+import SearchBox from "./Components/SearchBox";
 import "./App.css";
 
 class App extends Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
       robots: [],
-      searchfield: ''
-    }
+      searchfield: ""
+    };
   }
 
-  
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => {
+        this.setState({ robots: users });
+      });
   }
 
-
-  onSearchChange = (event) => {
-    console.log(event.target.value) ;
-    this.setState({ searchfield: event.target.value })
-  }
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
 
   render() {
-
     //filter the robots as user enter in searchbox and pass it to cardlist to display
-    const {robots , searchfield} = this.state;
-    const filteredRobots = robots.filter(robot =>{
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-
+    });
 
     return (
-      <div className="App bg-green">
-        <h1 className="f1 near-black tc">Robo Friend</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />
+      <div className="App bg-dark-gray">
+        <h1 className="f1 mt0 pt4 light-silver tc">Robo Friend</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     );
   }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,7 @@ import React from 'react' ;
 
 const Card = ({name , email}) => {
     return(
-         <div className="bg-moon-gray h6 dib tc pa2 br3 grow ba ma2 w5 b--solid"> 
+         <div className="bg-silver h6 dib tc pa2 br3 grow ba ma2 w5 b--solid"> 
              <img src={`https://robohash.org/${email}200x200`} alt="" />
              <h2 className="f4">{name}</h2>
              <h3 className="f5">{email}</h3>
