51f056203808f06c617ef7ff68ea785eed58f727
ashutoshjangra <ashutoshjangra603@gmail.com>
2018-12-29 11:51:38 +0530

    Updates

---
+++ precache-manifest.7566a836ce878e87aa81dc42b1f489c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1968003fb929cdf60084",
+    "url": "/Robo-Friend/static/js/runtime~main.1968003f.js"
+  },
+  {
+    "revision": "9339892c8a48383e5a0e",
+    "url": "/Robo-Friend/static/js/main.9339892c.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Robo-Friend/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "9339892c8a48383e5a0e",
+    "url": "/Robo-Friend/static/css/main.00dbcbae.chunk.css"
+  },
+  {
+    "revision": "af62613b87988e0aa80fb92933bb6fa7",
+    "url": "/Robo-Friend/index.html"
+  }
+];
