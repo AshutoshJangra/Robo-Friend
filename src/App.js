dbd1bbe4b3aa3e5ec323f9008283336bb0030b29
ashutoshjangra <ashutoshjangra603@gmail.com>
2018-12-30 20:00:22 +0530

    adding the custom robots file

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [],
+      robots: Robots,
       searchfield: ""
     };
   }
 
-  componentDidMount() {
+ /* componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
       .then(users => {
         this.setState({ robots: users });
       });
-  }
+  } */
 
   onSearchChange = event => {
     this.setState({ searchfield: event.target.value });
@@ -38,7 +38,7 @@ class App extends Component {
         <h1 className="f1 mt0 pt4 moon-gray tc">Robo Friend</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <CardList robots={robots} />
         </Scroll>
       </div>
     );
